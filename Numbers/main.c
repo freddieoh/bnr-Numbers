112dc8f3778afc808d60c1229d7ddd9ff517ff05
Fredrick Ohen <fredrickohen@gmail.com>
2017-10-17 15:30:46 -0500

    First commit

---
+++ Numbers/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Fredrick Ohen on 10/11/17.
+//  Copyright © 2017 geeoku. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+
+  
+//  printf("3 * 3 + 5 * 2 = %d. \n", 3 * 3 + 5 * 2);
+//  printf("11 / 3 = %d remainder of %d \n", 11 / 3, 11 % 3);
+//  printf("11 / 3.0 = %f\n", 11 / (float)3);
+//  printf("The absolute value of -5 is %d\n", abs(-5));
+//
+//
+//
+//  double y = 1234.6789;
+//  printf("y is %.2f\n", y);
+//  printf("y is %.2e\n", y);
+  
+  double sin(double x);
+  printf("The sin of x is %.2d\n", sin(6));
+  return 0;
+
+}
